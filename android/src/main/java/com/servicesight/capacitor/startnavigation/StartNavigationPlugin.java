de70343689a1df7b0a284e96830e4772d0d45c3d
Ashley Medway <ashley.medway@proteansoftware.co.uk>
2020-01-18 09:55:21 +0000

    Plugin should work for android now

--- android/src/main/java/com/servicesight/capacitor/startnavigation/StartNavigationPlugin.java
+++ android/src/main/java/com/servicesight/capacitor/startnavigation/StartNavigationPlugin.java
@@ -1,10 +1,13 @@
-package com.servicesight.capacitor-start-navigation;
+package com.servicesight.capacitor.startnavigation;
 
 import com.getcapacitor.JSObject;
 import com.getcapacitor.NativePlugin;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
+
+import android.content.Intent;
+import android.net.Uri;
 
 @NativePlugin()
 public class StartNavigationPlugin extends Plugin {
@@ -19,7 +22,7 @@ public class StartNavigationPlugin extends Plugin {
         Uri gmmIntentUri = Uri.parse("google.navigation:q=" + latitude + "," + longitude);
         Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
         mapIntent.setPackage("com.google.android.apps.maps");
-        startActivity(mapIntent);
+        getContext().startActivity(mapIntent);
 
         JSObject ret = new JSObject();
         ret.put("value", value);
