3cb466ca646b08b108c7a6f9dc6e389f971d76b3
Manuel Fernandez <46017940+manu-11098@users.noreply.github.com>
2021-12-14 09:54:24 +0100

    Update StartNavigationPlugin.java

--- android/src/main/java/com/servicesight/capacitor/startnavigation/StartNavigationPlugin.java
+++ android/src/main/java/com/servicesight/capacitor/startnavigation/StartNavigationPlugin.java
@@ -9,19 +9,27 @@ import com.getcapacitor.PluginMethod;
 import android.content.Intent;
 import android.net.Uri;
 
+import org.json.JSONException;
+
 @CapacitorPlugin()
 public class StartNavigationPlugin extends Plugin {
 
     @PluginMethod()
     public void launchMapsApp(PluginCall call) {
-        Double latitude = call.getDouble("latitude");
-        Double longitude = call.getDouble("longitude");
+        try {
+            Double latitude = call.getData().getDouble("latitude");
+            Double longitude = call.getData().getDouble("longitude");
+            Uri gmmIntentUri = Uri.parse("google.navigation:q=" + latitude + "," + longitude);
 
-        Uri gmmIntentUri = Uri.parse("google.navigation:q=" + latitude + "," + longitude);
-        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
-        mapIntent.setPackage("com.google.android.apps.maps");
-        getContext().startActivity(mapIntent);
+            Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+            mapIntent.setPackage("com.google.android.apps.maps");
+            getContext().startActivity(mapIntent);
 
-        call.success();
+            call.success();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
