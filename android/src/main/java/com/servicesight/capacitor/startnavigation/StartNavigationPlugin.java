5747d0d8a577eacef7fbdfd953d5f0fd21487488
Ashley Medway <ashley.medway@proteansoftware.co.uk>
2022-12-22 20:22:05 +0000

    Android implementation of address search

--- android/src/main/java/com/servicesight/capacitor/startnavigation/StartNavigationPlugin.java
+++ android/src/main/java/com/servicesight/capacitor/startnavigation/StartNavigationPlugin.java
@@ -1,27 +1,37 @@
 package com.servicesight.capacitor.startnavigation;
 
+import android.content.Intent;
+import android.net.Uri;
 import com.getcapacitor.JSObject;
-import com.getcapacitor.annotation.CapacitorPlugin;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
+import com.getcapacitor.annotation.CapacitorPlugin;
 
-import android.content.Intent;
-import android.net.Uri;
-
-@CapacitorPlugin()
+@CapacitorPlugin
 public class StartNavigationPlugin extends Plugin {
 
-    @PluginMethod()
+    @PluginMethod
     public void launchMapsApp(PluginCall call) {
-        Double latitude = call.getDouble("latitude");
-        Double longitude = call.getDouble("longitude");
+        Double latitude = call.getDouble("latitude", 0d);
+        Double longitude = call.getDouble("longitude", 0d);
+        String address = call.getString("address", null);
 
-        Uri gmmIntentUri = Uri.parse("google.navigation:q=" + latitude + "," + longitude);
+        if (latitude == 0d && longitude == 0d && address == null) {
+            call.reject("Must supply either: latitude & longitude or an address to search");
+        }
+
+        String query = "google.navigation:q=" + latitude + "," + longitude;
+
+        if (address != null) {
+            query += "?q=" + address.replace(' ', '+');
+        }
+
+        Uri gmmIntentUri = Uri.parse(query);
         Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
         mapIntent.setPackage("com.google.android.apps.maps");
         getContext().startActivity(mapIntent);
 
-        call.success();
+        call.resolve();
     }
 }
